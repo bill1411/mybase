4bffc6bcfb9b260cce1a229a11f966f81b85c397
15989077170 <346278334@qq.com>
2019-10-31 16:45:54 +0800

    增加pdf操作类

---
+++ Solution/PdfOperate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing.Imaging;
+
+namespace PdfOperate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            O2SPdfToImg();
+        }
+
+        private static void O2SPdfToImg()
+        {
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            int num = basePath.IndexOf("bin");
+            basePath = basePath.Substring(0, num);
+            O2SHelper.ConvertPDF2Image(basePath + @"file\demo.pdf", basePath + @"file\", "img_", 1, 2, ImageFormat.Jpeg, Definition.Five);
+        }
+    }
+}
