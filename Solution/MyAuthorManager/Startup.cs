90eb822b052bb5d0cb9e555c6113d46f7c01b7f2
15989077170 <346278334@qq.com>
2019-11-25 17:09:42 +0800

    添加依赖注入

--- Solution/MyAuthorManager/Startup.cs
+++ Solution/MyAuthorManager/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using MyCore;
 using MyDomain.IRepositories;
 using MyCore.Repositories;
+using MyApplications;
 
 namespace MyAuthorManager
 {
@@ -38,14 +39,25 @@ namespace MyAuthorManager
             var sqlConnectionString = Configuration.GetConnectionString("Default");
 
             //添加数据上下文
+            /*
+             * 注意：Asp.Net Core提供的依赖注入拥有三种生命周期模式，由短到长依次为：
+                Transient     ServiceProvider总是创建一个新的服务实例。
+                Scoped         ServiceProvider创建的服务实例由自己保存，（同一次请求）所以同一个ServiceProvider对象提供的服务实例均是同一个对象。
+                Singleton      始终是同一个实例对象
+             可通过第二个参数ServiceLifetime来控制数据库上下文对象的生命周期，默认为Scoped
+             */
+            //services.AddDbContext<MyDbContext>(options =>options.UseNpgsql(sqlConnectionString),ServiceLifetime.Scoped);
             services.AddDbContext<MyDbContext>(options =>options.UseNpgsql(sqlConnectionString));
 
+            //依赖注入
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserAppService, UserAppService>();
+
+            services.AddMvc();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             //Session服务
             services.AddSession();
-
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<IUserAppService, UserAppService>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
