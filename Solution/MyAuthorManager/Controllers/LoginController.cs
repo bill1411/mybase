0e929d549d427d6c4a10ae1dc6ff4e28f7ffba16
15989077170 <346278334@qq.com>
2019-11-25 17:10:16 +0800

    增加User服务类的注册

--- Solution/MyAuthorManager/Controllers/LoginController.cs
+++ Solution/MyAuthorManager/Controllers/LoginController.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyApplications;
 
 namespace MyAuthorManager.Controllers
 {
     public class LoginController : Controller
     {
+        //用户管理仓储接口
+        private IUserAppService _userAppService;
+
+
+        /// <summary>
+        /// 构造函数 实现依赖注入
+        /// </summary>
+        /// <param name="userRepository">仓储对象</param>
+        public LoginController(IUserAppService userAppService)
+        {
+            _userAppService = userAppService;
+        }
+
         public IActionResult Index()
         {
             return View();
