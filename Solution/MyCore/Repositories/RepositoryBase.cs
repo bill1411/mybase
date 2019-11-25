909e19ad6fdc94164413978ef63ba762c8f992f8
15989077170 <346278334@qq.com>
2019-11-25 10:52:37 +0800

    增加基础操作接口和用户操作实现接口

---
+++ Solution/MyCore/Repositories/RepositoryBase.cs
@@ -0,0 +1,214 @@
+﻿using MyDomain;
+using MyDomain.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MyCore.Repositories
+{
+    /// <summary>
+    /// 仓储基类
+    /// </summary>
+    /// <typeparam name="TEntity">实体类型</typeparam>
+    /// <typeparam name="TPrimaryKey">主键类型</typeparam>
+    public abstract class RepositoryBase<TEntity, TPrimaryKey> : IRepository<TEntity, TPrimaryKey> where TEntity : Entity<TPrimaryKey>
+    {
+        //定义数据访问上下文对象
+        protected readonly MyDbContext _dbContext;
+
+        /// <summary>
+        /// 通过构造函数注入得到数据上下文对象实例
+        /// </summary>
+        /// <param name="dbContext"></param>
+        public RepositoryBase(MyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// 获取实体集合
+        /// </summary>
+        /// <returns></returns>
+        public List<TEntity> GetAllList()
+        {
+            return _dbContext.Set<TEntity>().ToList();
+        }
+
+        /// <summary>
+        /// 根据lambda表达式条件获取实体集合
+        /// </summary>
+        /// <param name="predicate">lambda表达式条件</param>
+        /// <returns></returns>
+        public List<TEntity> GetAllList(Expression<Func<TEntity,bool>> predicate)
+        {
+            return _dbContext.Set<TEntity>().Where(predicate).ToList();
+        }
+
+        /// <summary>
+        /// 根据主键获取一个实体
+        /// </summary>
+        /// <param name="id">主键ID</param>
+        /// <returns></returns>
+        public TEntity Get(TPrimaryKey id)
+        {
+            return _dbContext.Set<TEntity>().FirstOrDefault(CreateEqualityExpressionForId(id));
+        }
+
+        /// <summary>
+        /// 根据lambda表达式条件获取一个实体
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbContext.Set<TEntity>().FirstOrDefault(predicate);
+        }
+
+        /// <summary>
+        /// 新增实体
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="autoSave">是否立即执行保存</param>
+        /// <returns></returns>
+        public TEntity Insert(TEntity entity, bool autoSave = true)
+        {
+            _dbContext.Set<TEntity>().Add(entity);
+            if (autoSave)
+                Save();
+            return entity;
+        }
+
+        /// <summary>
+        /// 更新实体
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="autoSave">是否立即执行保存</param>
+        /// <returns></returns>
+        public TEntity Update(TEntity entity, bool autoSave = true)
+        {
+            var obj = Get(entity.Id);
+            EntityToEntity(entity, obj);
+            if (autoSave)
+                Save();
+            return entity;
+        }
+
+        /// <summary>
+        /// 新增或更新实体
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="autoSave">是否立即执行保存</param>
+        public TEntity InsertOrUpdate(TEntity entity, bool autoSave = true)
+        {
+            if (Get(entity.Id) != null)  //如果找不到，说明是新增
+                return Update(entity, autoSave);
+            return Insert(entity, autoSave);
+        }
+
+        /// <summary>
+        /// 删除实体
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="autoSave">是否立即执行保存</param>
+        public void Delete(TEntity entity, bool autoSave = true)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+            if (autoSave)
+                Save();
+        }
+
+        public void Delete(TPrimaryKey id, bool autoSave = true)
+        {
+            _dbContext.Set<TEntity>().Remove(Get(id));
+            if (autoSave)
+                Save();
+        }
+
+        /// <summary>
+        /// 根据条件删除实体集
+        /// </summary>
+        /// <param name="where">lambda表达式</param>
+        /// <param name="autoSave">是否立即执行保存</param>
+        public void Delete(Expression<Func<TEntity, bool>> where, bool autoSave = true)
+        {
+            _dbContext.Set<TEntity>().Where(where).ToList().ForEach(it => _dbContext.Set<TEntity>().Remove(it));
+            if (autoSave)
+                Save();
+        }
+
+        /// <summary>
+        /// 分页查询
+        /// </summary>
+        /// <param name="pageCount">当前是第几页</param>
+        /// <param name="pageSize">每次取多少条</param>
+        /// <param name="rowCount">行数</param>
+        /// <param name="where">条件</param>
+        /// <param name="order">排序</param>
+        /// <returns></returns>
+        public IQueryable<TEntity> LoadPageList(int pageCount, int pageSize, out int rowCount, Expression<Func<TEntity, bool>> where = null, Expression<Func<TEntity, object>> order = null)
+        {
+            var result = from p in _dbContext.Set<TEntity>()
+                         select p;
+            if (where != null)
+                result = result.Where(where);
+            if (order != null)
+                result = result.OrderBy(order);
+            else
+                result = result.OrderBy(m => m.Id);
+            rowCount = result.Count();
+            return result.Skip((pageCount - 1) * pageSize).Take(pageSize);
+        }
+
+
+        /// <summary>
+        /// 将新实体的值，循环赋值给旧实体
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="pTargetObjSrc">新实体</param>
+        /// <param name="pTargetObjDest">旧实体</param>
+        private void EntityToEntity<T>(T pTargetObjSrc, T pTargetObjDest)
+        {
+            foreach (var mItem in typeof(T).GetProperties())
+            {
+                mItem.SetValue(pTargetObjDest, mItem.GetValue(pTargetObjSrc, new object[] { }), null);
+            }
+        }
+
+        /// <summary>
+        /// 事务性保存
+        /// </summary>
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// 根据主键构建判断表达式
+        /// </summary>
+        /// <param name="id">主键</param>
+        /// <returns></returns>
+        protected static Expression<Func<TEntity, bool>> CreateEqualityExpressionForId(TPrimaryKey id)
+        {
+            var lambdaParam = Expression.Parameter(typeof(TEntity));
+            var lambdaBody = Expression.Equal(
+                Expression.PropertyOrField(lambdaParam, "Id"),
+                Expression.Constant(id, typeof(TPrimaryKey))
+                );
+
+            return Expression.Lambda<Func<TEntity, bool>>(lambdaBody, lambdaParam);
+        }
+    }
+
+    /// <summary>
+    /// 主键为Guid类型的仓储基类
+    /// </summary>
+    /// <typeparam name="TEntity">实体类型</typeparam>
+    public abstract class RepositoryBase<TEntity> : RepositoryBase<TEntity, Guid> where TEntity : Entity
+    {
+        public RepositoryBase(MyDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ Solution/MyCore/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using MyDomain.Entities;
+using MyDomain.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyCore.Repositories
+{
+    public class UserRepository:RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(MyDbContext dbcontext) : base(dbcontext)
+        {
+
+        }
+
+        /// <summary>
+        /// 根据用户名及密码获取用户信息
+        /// </summary>
+        /// <param name="userName">用户名</param>
+        /// <param name="password">密码</param>
+        /// <returns></returns>
+        public User CheckUser(string userName, string password)
+        {
+            return _dbContext.Set<User>().FirstOrDefault(it=>it.Password == password && it.UserName == userName);
+        }
+
+        /// <summary>
+        /// 根据Id获取实体及角色信息
+        /// </summary>
+        /// <param name="id">Id</param>
+        /// <returns></returns>
+        public User GetWithRoles(Guid id)
+        {
+            var user = _dbContext.Set<User>().FirstOrDefault(it => it.Id == id);
+            if (user != null)
+            {
+                user.UserRoles = _dbContext.Set<UserRole>().Where(it => it.UserId == id).ToList();
+            }
+            return user;
+        }
+    }
+}
