49e370c2932332568a19011fd8f30f49a6dc0435
15989077170 <346278334@qq.com>
2019-11-26 16:15:11 +0800

    增加一个抽象类 其他collector都继承自此类，可以省略在每个collctor里写拦截方法
    用来拦截页面访问，判断如果没有session则直接跳转到login\index页面

---
+++ Solution/MyAuthorManager/Controllers/ControllerBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MyAuthorManager.Controllers
+{
+    public abstract class ControllerBase : Controller
+    {
+        /// <summary>
+        /// 控制器拦截，就是当我们直接通过在地址栏输入访问路由地址时，首先应该判断用户是否已经登录
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            byte[] result;
+            string ss = HttpContext.Session.GetString("CurrentUserId");
+            filterContext.HttpContext.Session.TryGetValue("CurrentUser", out result);
+            if (result == null)
+            {
+                filterContext.Result = new RedirectResult("/Login/Index");
+                return;
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
