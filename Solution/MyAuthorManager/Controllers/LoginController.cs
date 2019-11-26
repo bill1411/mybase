8f2187280d60718ff1791fb7fa1fe1544f77a4ef
15989077170 <346278334@qq.com>
2019-11-26 16:23:20 +0800

    增加登陆功能

--- Solution/MyAuthorManager/Controllers/LoginController.cs
+++ Solution/MyAuthorManager/Controllers/LoginController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyApplications;
+using MyAuthorManager.Models;
+using MyUtility;
 
 namespace MyAuthorManager.Controllers
 {
@@ -24,6 +27,34 @@ namespace MyAuthorManager.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// 用户登陆
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult Index(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //检查用户信息
+                var user = _userAppService.CheckUser(model.UserName, model.Password);
+                if (user != null)//说明有用户信息
+                {
+                    //记录Session
+                    HttpContext.Session.SetString("CurrentUserId", user.Id.ToString());
+                    HttpContext.Session.Set("CurrentUser", ByteConvertHelper.Object2Bytes(user));
+                    //跳转到系统首页
+                    return RedirectToAction("Index", "Home");
+                }
+                ViewBag.ErrorInfo = "用户名或密码错误。";
+                return View();
+            }
+            ViewBag.ErrorInfo = ModelState.Values.First().Errors[0].ErrorMessage;
+            return View(model);
         }
     }
 }
