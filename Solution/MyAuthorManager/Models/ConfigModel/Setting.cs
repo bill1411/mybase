1ad87098b257b75c115727aac86358a0bece7cdc
15989077170 <346278334@qq.com>
2019-11-26 16:22:48 +0800

    增加用户登陆model，面向页面的

---
+++ Solution/MyAuthorManager/Models/ConfigModel/Setting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAuthorManager.Models.ConfigModel
+{
+    public class Setting
+    {
+        /// <summary>
+        /// session过期时间（秒）
+        /// </summary>
+        public string session_timeout { get; set; }
+    }
+}
---
+++ Solution/MyAuthorManager/Models/LoginModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAuthorManager.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "用户名不能为空")]
+        [DataType(DataType.Text)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "密码不能为空")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        /// <summary>
+        /// 客户是否勾选了记住密码选项
+        /// </summary>
+        public bool RememberMe { get; set; }
+    }
+}
