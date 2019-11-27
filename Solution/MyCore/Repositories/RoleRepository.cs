fbef88c12c9df172f0116917f526eadf2341eb3c
15989077170 <346278334@qq.com>
2019-11-27 16:14:46 +0800

    更新底层方法

--- Solution/MyCore/Repositories/RoleRepository.cs
+++ Solution/MyCore/Repositories/RoleRepository.cs
@@ -1,10 +1,43 @@
-﻿using System;
+﻿using MyDomain.Entities;
+using MyDomain.IRepositories;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MyCore.Repositories
 {
-    class RoleRepository
+    public class RoleRepository:RepositoryBase<Role>, IRoleRepository
     {
+        public RoleRepository(MyDbContext dbcontext) : base(dbcontext)
+        {
+
+        }
+        /// <summary>
+        /// 根据角色获取权限
+        /// </summary>
+        /// <returns></returns>
+        public List<Guid> GetAllMenuListByRole(Guid roleId)
+        {
+            var roleMenus = _dbContext.Set<RoleMenu>().Where(it => it.RoleId == roleId);
+            var menuIds = from t in roleMenus select t.MenuId;
+            return menuIds.ToList();
+        }
+
+        /// <summary>
+        /// 更新角色权限关联关系
+        /// </summary>
+        /// <param name="roleId">角色id</param>
+        /// <param name="roleMenus">角色权限集合</param>
+        /// <returns></returns>
+        public bool UpdateRoleMenu(Guid roleId, List<RoleMenu> roleMenus)
+        {
+            var oldDatas = _dbContext.Set<RoleMenu>().Where(it => it.RoleId == roleId).ToList();
+            oldDatas.ForEach(it => _dbContext.Set<RoleMenu>().Remove(it));
+            _dbContext.SaveChanges();
+            _dbContext.Set<RoleMenu>().AddRange(roleMenus);
+            _dbContext.SaveChanges();
+            return true;
+        }
     }
 }
