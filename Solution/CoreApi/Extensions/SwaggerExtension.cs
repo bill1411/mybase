a4bc21225a09ae32ea84c2209b87a3e61943f00b
15989077170 <346278334@qq.com>
2020-01-06 17:04:42 +0800

    增加扩展方法

---
+++ Solution/CoreApi/Extensions/SwaggerExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace SwaggerCoreApi.Extensions
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class SwaggerExtension
+    {
+        /// <summary>
+        /// 添加控制器swagger扩展类
+        /// </summary>
+        public class ApplyTagDescriptions : IDocumentFilter
+        {
+            /// <summary>
+            /// swagger汉化标签
+            /// </summary>
+            /// <param name="swaggerDoc"></param>
+            /// <param name="context"></param>
+            public void Apply(SwaggerDocument swaggerDoc, DocumentFilterContext context)
+            {
+                //Name就是接口的大类   Description就是关于该类的描述
+                swaggerDoc.Tags = new List<Tag>
+                {
+                    new Tag { Name = "Values", Description = "企业相关接口" },
+                    //new Tag { Name = "Ticket", Description = "机票相关接口" }
+                };
+            }
+        }
+    }
+
+}
