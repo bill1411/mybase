7140a5357f3d292d2afe33a6526deba6b679e3ab
15989077170 <346278334@qq.com>
2020-01-06 11:40:37 +0800

    新增asp.net framework webapi配置swagger

---
+++ Solution/FrameworkApi/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace FrameworkApi.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ Solution/FrameworkApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FrameworkApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
